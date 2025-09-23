e211ec8afa5c0e3b591be4b06c26b524bfddf8b5
Pawel Chmielewski <p.t.chmielewski@gmail.com>
2025-09-23 09:43:14 +0200

    feat: Removed unit test case

--- lib_test.go
+++ lib_test.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"testing"
 	"time"
-
-	"github.com/open-crypto-broker/crypto-broker-client-go/internal/protobuf"
-	"google.golang.org/grpc"
 )
 
 func TestNewLibrary(t *testing.T) {
@@ -32,45 +29,6 @@ func TestNewLibrary(t *testing.T) {
 			if (err != nil) != tt.wantErr {
 				t.Errorf("NewLibrary() error = %v, wantErr %v", err, tt.wantErr)
 				return
-			}
-		})
-	}
-}
-
-func TestLibrary_Close(t *testing.T) {
-	workingLib, err := NewLibrary(context.Background())
-	if err != nil {
-		t.Errorf("could not instantiate working instance of library, err: %s", err)
-
-		return
-	}
-
-	type fields struct {
-		client protobuf.CryptoBrokerClient
-		conn   *grpc.ClientConn
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		wantErr bool
-	}{
-		{
-			name: "Close() succeeds on correctly initialized Library",
-			fields: fields{
-				client: &mockedCryptoBrokerClient{},
-				conn:   workingLib.conn,
-			},
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			lib := &Library{
-				client: tt.fields.client,
-				conn:   tt.fields.conn,
-			}
-			if err := lib.Close(); (err != nil) != tt.wantErr {
-				t.Errorf("Library.Close() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
