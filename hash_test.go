bcc289445dc16a4207961fbe036a3e3d3173e038
Paweł Chmielewski <pawel.chmielewski@sap.com>
2026-06-23 08:48:40 +0200

    fix: Linted code

--- hash_test.go
+++ hash_test.go
@@ -83,8 +83,8 @@ func TestLibrary_HashData(t *testing.T) {
 			args: args{
 				ctx: context.TODO(),
 				payload: HashDataPayload{
-					Profile:          "Default",
-					Input:            []byte("Hello world"),
+					Profile:      "Default",
+					Input:        []byte("Hello world"),
 					OutputFormat: OutputFormatRaw,
 					Metadata: &Metadata{
 						Id: "123",
